1741944c1552e37f4761d8a3db0463661db47dc6
Ricardo Constantino Ferreira <=>
2026-03-19 11:11:49 -0300

    create new config

---
+++ src/main/java/com/ferreira/auto/infra/configuration/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.ferreira.auto.infra.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment environment;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Libera todos os caminhos da API
+                .allowedOrigins(environment.getProperty("FERREIRA_AUTO_URL_FRONT", ""))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
